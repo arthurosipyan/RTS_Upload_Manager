73eb942c7fa80236ea4eb4199f32991ad6a7ae52
Arthur Osipyan <33210928+arthurosipyan@users.noreply.github.com>
2022-05-08 16:20:58 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,64 @@
-// Method to upload a valid excel file
 function upload() {
-    // let element = document.getElementById("copyTextButton");
-    // let hidden = element.getAttribute("hidden");
-
-    // if (hidden) {
-    //    element.removeAttribute("hidden");
-    // } else {
-    //    element.setAttribute("hidden", "hidden");
-    // }
-
     document.getElementById("copyTextButton").style.display = "block";
-    
     let files = document.getElementById('fileUpload').files;
-    if(files.length==0){
+    
+    if (files.length==0) {
         alert("Please choose any file...");
         return;
     }
+
     let filename = files[0].name;
     let extension = filename.substring(filename.lastIndexOf(".")).toUpperCase();
+    
     if (extension == '.XLS' || extension == '.XLSX') {
         processFile(files[0]);
-    }else{
+    }
+    else {
         alert("Please select a valid excel file.");
     }
-  }
-  
-  function copyText() {
+}
+
+function copyText() {
     /* Get the text field */
     let copiedText = document.getElementById("sheetResults");
   
     /* Select the text field */
     copiedText.select();
     copiedText.setSelectionRange(0, 99999); /* For mobile devices */
-  
-     /* Copy the text inside the text field */
+    
+    /* Copy the text inside the text field */
     navigator.clipboard.writeText(copiedText.value);
   
     /* Alert the copied text */
     alert("Copied the text: " + copiedText.value);
-  }
+}
 
-function getTotalRows(invoices){
+function getTotalRows(invoices) {
     return invoices.length;
 }
 
-function getTotalAmount(sheet){
+function getTotalAmount(sheet) {
     let x = 0;
     let total = 0;
-    while (x < sheet.length){
+    while (x < sheet.length) {
         total += sheet[x]['InvAmt'];
         x++;
     }
     return total;
 }
 
-function getInvoices(sheet){
+function getInvoices(sheet) {
     let x = 0;
     let invoiceArr = [];
-    while (x < sheet.length){
+    while (x < sheet.length) {
         invoiceArr.push('"' + sheet[x]['Invoice#'] + '"');
         x++;
     }
     return invoiceArr;
 }
 
-// Method to read excel file and convert it into JSON 
-function processFile(file){
+// Method to read and process excel file
+function processFile(file) {
     try {
         let reader = new FileReader();
         reader.readAsBinaryString(file);
@@ -83,7 +76,7 @@ function processFile(file){
                 if (roa.length > 0) {
                     result[sheetName] = roa;
                 }
-              });
+            });
 
             // get list of invoices
             let invoiceList = getInvoices(result[lastSheet]);
@@ -95,7 +88,7 @@ function processFile(file){
             let totalAmount = (getTotalAmount(result[lastSheet])).toLocaleString('en-US', {
                 style: 'currency',
                 currency: 'USD',
-              });;
+            });;
             
             // display results
             var sheetStats = document.getElementById("totalInvoices");
@@ -106,12 +99,10 @@ function processFile(file){
 
             var resultEle=document.getElementById("sheetResults");
             resultEle.textContent = invoiceList;
-            // resultEle.value=JSON.stringify(result[lastSheet], null, 4);
             resultEle.style.display='block';
-            
-            }
-            
-      }catch(e){
-          console.error(e);
-      }
+        }
+    
+    }catch(e) {
+        console.error(e);
+    }
 }
